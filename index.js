11443cdb3f2cdb02fb5d35e01d522f548df14be5
Ilayda Guzel <ilaydanurguzel1@gmail.com>
2023-03-27 12:55:54 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+
+//1
+function findMatching(drivers,names) {
+    return drivers.filter(function(driver)
+    { return driver.toLowerCase() === names.toLowerCase() });
+  };
+
+// findMatching(drivers,"Bobby");
+// findMatching(drivers,"Sammy");
+
+
+
+//2
+function fuzzyMatch(drivers,names) {
+    return drivers.filter(function(driver)
+    { return driver.toLowerCase().substring(0, names.length) === names.toLowerCase() });
+  };
+
+
+
+//3
+function matchName(drivers,arrg) {
+    return drivers.filter(function(driver)
+    {return driver.name===arrg});
+  };
+
+
+
